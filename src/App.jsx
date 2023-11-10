ffe85d51c3128f63aec19805b28806fae1ade696
amanueltamer <amanueltamer@gmail.com>
2023-11-10 06:34:11 -0600

    Removed ResultsPage from App.jsx, not using it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ResultsPage from "./components/ResultsPage";
 import PeoplePage from "./components/PeoplePage";
 import TrendingPage from "./components/TrendingPage";
 import Upcomingpage from "./components/UpcomingPage";
@@ -20,7 +19,6 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/trending" element={<TrendingPage />}></Route>
         <Route path="/discover" element={<DiscoverPage />}></Route>
-        <Route path="/resultspage" element={<ResultsPage />}></Route>
         <Route path="/people" element={<PeoplePage />}></Route>
         <Route path="/upcoming" element={<Upcomingpage />}></Route>
         <Route path="/topRated" element={<TopRatedPage />}></Route>
