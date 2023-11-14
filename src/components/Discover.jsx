ddb9609aceeba07cc413b76cf21a6c5518258823
amanueltamer <amanueltamer@gmail.com>
2023-11-14 11:37:10 -0600

    Removing Unused Code

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -13,15 +13,12 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
 export default function Discover() {
-  const [trending, setTrending] = useState();
   const [data, setData] = useState([]);
   const [currentPage, setCurrentPage] = useState(0);
   const [totalPages, setTotalPages] = useState(0);
   const [subset, setSubset] = useState([]);
   const [selectedMovieValue, setSelectedMovieValue] = useState("28");
   const [selectedTvValue, setSelectedTvValue] = useState("10759");
-  const [movieGenres, setMovieGenres] = useState([]);
-  const [tvGenres, setTvGenres] = useState([]);
   const navigate = useNavigate();
   const isSmallScreen = useMediaQuery("(max-width: 1184px)");
 
@@ -59,58 +56,28 @@ export default function Discover() {
         const totalPagesToShow = Math.min(data.total_pages, 500);
         setTotalPages(totalPagesToShow);
         if (isSmallScreen) {
-            setSubset(dataExtract.slice(0, itemsPerPage));
-            } else {
-              setSubset(dataExtract.slice(0, itemsPerPage));
-            }
+          setSubset(dataExtract.slice(0, itemsPerPage));
+        } else {
+          setSubset(dataExtract.slice(0, itemsPerPage));
+        }
       } else {
-        // No data available for this page, you can handle this case as needed.
-        // For example, displaying a message or doing nothing.
         console.log("No data available for this page.");
       }
     } catch (error) {
-      // Handle errors appropriately.
       console.error("Error fetching discover data:", error);
     }
   }
 
-  //   const getMovieGenres = async () => {
-  //     try {
-  //       const { data } = await axios.get(
-  //         `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`
-  //       );
-
-  //       setMovieGenres(data.genres);
-
-  //       // Process and use the movie genres data as needed.
-  //       console.log("Movie Genres:", movieGenres);
-  //     } catch (error) {
-  //       // Handle errors appropriately.
-  //       console.error("Error fetching movie genres data:", error);
-  //     }
-  //   };
-
-  //   const getTvGenres = async () => {
-  //     try {
-  //       const { data } = await axios.get(
-  //         `https://api.themoviedb.org/3/genre/tv/list?api_key=${API_KEY}`
-  //       );
-
-  //       setTvGenres(data.genres);
-
-  //       // Process and use the tv genres data as needed.
-  //       console.log("Tv Genres:", tvGenres);
-  //     } catch (error) {
-  //       // Handle errors appropriately.
-  //       console.error("Error fetching tv genres data:", error);
-  //     }
-  //   };
-
   useEffect(() => {
     getDiscover(currentPage + 1);
-    // getMovieGenres();
-    // getTvGenres();
-  }, [currentPage, selectedMovieValue, selectedTvValue, mediaType, totalPages, isSmallScreen]);
+  }, [
+    currentPage,
+    selectedMovieValue,
+    selectedTvValue,
+    mediaType,
+    totalPages,
+    isSmallScreen,
+  ]);
 
   const handlePageChange = (selectedPage) => {
     setCurrentPage(selectedPage.selected);
@@ -143,7 +110,6 @@ export default function Discover() {
 
   console.log(selectedMovieValue);
   console.log(selectedTvValue);
-  //   console.log(movieGenres);
   console.log("Media Type", mediaType);
   console.log("Prev Media Type", prevMediaType);
   console.log(data);
@@ -157,90 +123,98 @@ export default function Discover() {
                 <h1>Discover</h1>
               </div>
               <div className="trending__buttonGroup">
-
-              <ToggleButtonGroup
-                value={mediaType}
-                exclusive
-                onChange={(event, newMediaType) =>
-                  handleMediaTypeToggle(newMediaType)
-                }
-                className="trending__buttons"
-              >
-                <ToggleButton
-                  value="movie"
-                  aria-label="Movies"
-                  className={
-                    mediaType === "movie" || (mediaType === "movie" && prevMediaType === "tv") || (mediaType === "movie" && prevMediaType === null) || (mediaType === null && prevMediaType === "movie")
-                      ? "selectedButton"
-                      : "customToggleButton"
+                <ToggleButtonGroup
+                  value={mediaType}
+                  exclusive
+                  onChange={(event, newMediaType) =>
+                    handleMediaTypeToggle(newMediaType)
                   }
+                  className="trending__buttons"
                 >
-                  Movies
-                </ToggleButton>
-                <ToggleButton
-                  value="tv"
-                  aria-label="TV Shows"
-                  className={
-                    mediaType === "tv" || (mediaType === "tv" && prevMediaType === "movie") || (mediaType === "tv" && prevMediaType === null) || (mediaType === null && prevMediaType === "tv") ? "selectedButton" : "customToggleButton"
-                  }
-                >
-                  TV Shows
-                </ToggleButton>
-              </ToggleButtonGroup>
-              {mediaType === "movie" || (prevMediaType === "movie" && mediaType !== "tv") ? (
-                <Select
-                  value={selectedMovieValue}
-                  onChange={handleSelectChange}
-                  label="Media Type"
-                  variant="outlined"
-                  className="trending__select"
-                >
-                  <MenuItem value="28">Action</MenuItem>
-                  <MenuItem value="12">Adventure</MenuItem>
-                  <MenuItem value="16">Animation</MenuItem>
-                  <MenuItem value="35">Comedy</MenuItem>
-                  <MenuItem value="80">Crime</MenuItem>
-                  <MenuItem value="99">Documentary</MenuItem>
-                  <MenuItem value="18">Drama</MenuItem>
-                  <MenuItem value="10751">Family</MenuItem>
-                  <MenuItem value="14">Fantasy</MenuItem>
-                  <MenuItem value="36">History</MenuItem>
-                  <MenuItem value="27">Horror</MenuItem>
-                  <MenuItem value="10402">Music</MenuItem>
-                  <MenuItem value="9648">Mystery</MenuItem>
-                  <MenuItem value="10749">Romance</MenuItem>
-                  <MenuItem value="878">Science Fiction</MenuItem>
-                  <MenuItem value="10770">Tv Movie</MenuItem>
-                  <MenuItem value="53">Thriller</MenuItem>
-                  <MenuItem value="10752">War</MenuItem>
-                  <MenuItem value="37">Western</MenuItem>
-                </Select>
-              ) : (
-                <Select
-                  value={selectedTvValue}
-                  onChange={handleSelectChange}
-                  label="Media Type"
-                  variant="outlined"
-                  className="trending__select"
-                >
-                  <MenuItem value="10759">Action & Adventure</MenuItem>
-                  <MenuItem value="16">Animation</MenuItem>
-                  <MenuItem value="35">Comedy</MenuItem>
-                  <MenuItem value="80">Crime</MenuItem>
-                  <MenuItem value="99">Documentary</MenuItem>
-                  <MenuItem value="18">Drama</MenuItem>
-                  <MenuItem value="10751">Family</MenuItem>
-                  <MenuItem value="10762">Kids</MenuItem>
-                  <MenuItem value="9648">Mystery</MenuItem>
-                  <MenuItem value="10763">News</MenuItem>
-                  <MenuItem value="10764">Reality</MenuItem>
-                  <MenuItem value="10765">Sci-fi & Fantasy</MenuItem>
-                  <MenuItem value="10766">Soap</MenuItem>
-                  <MenuItem value="10767">Talk</MenuItem>
-                  <MenuItem value="10768">War & Politics</MenuItem>
-                  <MenuItem value="37">Western</MenuItem>
-                </Select>
-              )}
+                  <ToggleButton
+                    value="movie"
+                    aria-label="Movies"
+                    className={
+                      mediaType === "movie" ||
+                      (mediaType === "movie" && prevMediaType === "tv") ||
+                      (mediaType === "movie" && prevMediaType === null) ||
+                      (mediaType === null && prevMediaType === "movie")
+                        ? "selectedButton"
+                        : "customToggleButton"
+                    }
+                  >
+                    Movies
+                  </ToggleButton>
+                  <ToggleButton
+                    value="tv"
+                    aria-label="TV Shows"
+                    className={
+                      mediaType === "tv" ||
+                      (mediaType === "tv" && prevMediaType === "movie") ||
+                      (mediaType === "tv" && prevMediaType === null) ||
+                      (mediaType === null && prevMediaType === "tv")
+                        ? "selectedButton"
+                        : "customToggleButton"
+                    }
+                  >
+                    TV Shows
+                  </ToggleButton>
+                </ToggleButtonGroup>
+                {mediaType === "movie" ||
+                (prevMediaType === "movie" && mediaType !== "tv") ? (
+                  <Select
+                    value={selectedMovieValue}
+                    onChange={handleSelectChange}
+                    label="Media Type"
+                    variant="outlined"
+                    className="trending__select"
+                  >
+                    <MenuItem value="28">Action</MenuItem>
+                    <MenuItem value="12">Adventure</MenuItem>
+                    <MenuItem value="16">Animation</MenuItem>
+                    <MenuItem value="35">Comedy</MenuItem>
+                    <MenuItem value="80">Crime</MenuItem>
+                    <MenuItem value="99">Documentary</MenuItem>
+                    <MenuItem value="18">Drama</MenuItem>
+                    <MenuItem value="10751">Family</MenuItem>
+                    <MenuItem value="14">Fantasy</MenuItem>
+                    <MenuItem value="36">History</MenuItem>
+                    <MenuItem value="27">Horror</MenuItem>
+                    <MenuItem value="10402">Music</MenuItem>
+                    <MenuItem value="9648">Mystery</MenuItem>
+                    <MenuItem value="10749">Romance</MenuItem>
+                    <MenuItem value="878">Science Fiction</MenuItem>
+                    <MenuItem value="10770">Tv Movie</MenuItem>
+                    <MenuItem value="53">Thriller</MenuItem>
+                    <MenuItem value="10752">War</MenuItem>
+                    <MenuItem value="37">Western</MenuItem>
+                  </Select>
+                ) : (
+                  <Select
+                    value={selectedTvValue}
+                    onChange={handleSelectChange}
+                    label="Media Type"
+                    variant="outlined"
+                    className="trending__select"
+                  >
+                    <MenuItem value="10759">Action & Adventure</MenuItem>
+                    <MenuItem value="16">Animation</MenuItem>
+                    <MenuItem value="35">Comedy</MenuItem>
+                    <MenuItem value="80">Crime</MenuItem>
+                    <MenuItem value="99">Documentary</MenuItem>
+                    <MenuItem value="18">Drama</MenuItem>
+                    <MenuItem value="10751">Family</MenuItem>
+                    <MenuItem value="10762">Kids</MenuItem>
+                    <MenuItem value="9648">Mystery</MenuItem>
+                    <MenuItem value="10763">News</MenuItem>
+                    <MenuItem value="10764">Reality</MenuItem>
+                    <MenuItem value="10765">Sci-fi & Fantasy</MenuItem>
+                    <MenuItem value="10766">Soap</MenuItem>
+                    <MenuItem value="10767">Talk</MenuItem>
+                    <MenuItem value="10768">War & Politics</MenuItem>
+                    <MenuItem value="37">Western</MenuItem>
+                  </Select>
+                )}
               </div>
             </div>
             <div className="trending__cards">
@@ -254,12 +228,9 @@ export default function Discover() {
                             src={IMG_API + movie.poster_path}
                             alt=""
                             onClick={() => {
-                    
                               if (movie.title) {
-                                // Use title as a route parameter
                                 navigate(`/movie/${movie.id}/${movie.title}`);
                               } else if (movie.name) {
-                                // Use name as a route parameter
                                 navigate(`/show/${movie.id}/${movie.name}`);
                               }
                             }}
