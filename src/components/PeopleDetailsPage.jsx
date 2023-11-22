6bbf0425d462a5cc7633f011ab1c1aa9f8ca2f9e
amanueltamer <amanueltamer@gmail.com>
2023-11-21 21:02:32 -0600

    Adding PeopleDetailsPage

---
+++ src/components/PeopleDetailsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+import PeopleDetails from './PeopleDetails'
+
+export default function PeopleDetailsPage() {
+  return (
+    <div className='peopleDetailsPage'>
+        <Header />
+        <PeopleDetails />
+        <Footer />
+    </div>
+  )
+}
