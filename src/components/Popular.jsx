cc89aecdd29a571c539ce2ee4815478eafdf7288
amanueltamer <amanueltamer@gmail.com>
2023-12-02 06:24:09 -0600

    Properly encoding links

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -60,9 +60,9 @@ const Popular = () => {
                         alt={movie.name}
                         onClick={() => {
                           if (movie.title) {
-                            navigate(`/movie/${movie.id}/${movie.title}`);
+                            navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
                           } else if (movie.name) {
-                            navigate(`/show/${movie.id}/${movie.name}`);
+                            navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
                           }
                         }}
                       />
