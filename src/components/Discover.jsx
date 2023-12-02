f20efd3ae27b806ba4bc0521c25c977de04f72ce
amanueltamer <amanueltamer@gmail.com>
2023-12-02 06:19:54 -0600

    Properly encoding links

--- src/components/Discover.jsx
+++ src/components/Discover.jsx
@@ -221,9 +221,9 @@ export default function Discover() {
                             alt=""
                             onClick={() => {
                               if (movie.title) {
-                                navigate(`/movie/${movie.id}/${movie.title}`);
+                                navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
                               } else if (movie.name) {
-                                navigate(`/show/${movie.id}/${movie.name}`);
+                                navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
                               }
                             }}
                           />
