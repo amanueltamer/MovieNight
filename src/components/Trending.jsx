0ebe473c3c449350e6c40cc06ddc39a8e115fbaa
amanueltamer <amanueltamer@gmail.com>
2023-12-02 06:24:38 -0600

    Properly encoding links

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -125,9 +125,9 @@ export default function Trending() {
                           alt=""
                           onClick={() => {
                             if (movie.title) {
-                              navigate(`/movie/${movie.id}/${movie.title}`);
+                              navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
                             } else if (movie.name) {
-                              navigate(`/show/${movie.id}/${movie.name}`);
+                              navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
                             }
                           }}
                         />
