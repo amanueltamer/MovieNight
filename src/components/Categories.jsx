8dfe760c1fad3dc78f74814615e555ce727fbed7
amanueltamer <amanueltamer@gmail.com>
2023-12-02 06:19:46 -0600

    Properly encoding links

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -106,9 +106,9 @@ export default function Categories() {
                     alt={movie.title}
                     onClick={() => {
                       if (movie.title) {
-                        navigate(`/movie/${movie.id}/${movie.title}`);
+                        navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
                       } else if (movie.name) {
-                        navigate(`/show/${movie.id}/${movie.name}`);
+                        navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
                       }
                     }}
                   />
@@ -153,9 +153,9 @@ export default function Categories() {
                     alt={movie.title}
                     onClick={() => {
                       if (movie.title) {
-                        navigate(`/movie/${movie.id}/${movie.title}`);
+                        navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
                       } else if (movie.name) {
-                        navigate(`/show/${movie.id}/${movie.name}`);
+                        navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
                       }
                     }}
                   />
@@ -200,9 +200,9 @@ export default function Categories() {
                     alt={movie.title}
                     onClick={() => {
                       if (movie.title) {
-                        navigate(`/movie/${movie.id}/${movie.title}`);
+                        navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
                       } else if (movie.name) {
-                        navigate(`/show/${movie.id}/${movie.name}`);
+                        navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
                       }
                     }}
                   />
