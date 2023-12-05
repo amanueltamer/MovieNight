6bc54040a3d212dc1a296091a233b4064907aea7
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:41:20 -0600

    Reformatting code

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const Footer = () => {
-    return (
-        <div className='footer'>
-            <p className='footer__center'>
-                All rights reserved © 2023 Amanuel Tamer
-            </p>
-        </div>
-    );
-}
+  return (
+    <div className="footer">
+      <p className="footer__center">All rights reserved © 2023 Amanuel Tamer</p>
+    </div>
+  );
+};
 
 export default Footer;
