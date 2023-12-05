2c8e6f9dc6f8cd3dcdca1d5bbf8a073effa8d0a7
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:41:53 -0600

    Reformatting code

--- src/components/PeoplePage.jsx
+++ src/components/PeoplePage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
-import People from './People'
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import People from "./People";
 
 export default function PeoplePage() {
   return (
-    <div className='people__page'>
-        <Header />
-        <People />
-        <Footer />
+    <div className="people__page">
+      <Header />
+      <People />
+      <Footer />
     </div>
-  )
+  );
 }
