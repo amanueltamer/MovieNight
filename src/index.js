86471834087964d1e6d03a9b073fcc68f88e7962
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:40:34 -0600

    Reformatting code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux'; // Import the Provider component
-import store from '../src/app/store'; // Import your Redux store
-import App from './App'; // Import your root component
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux"; // Import the Provider component
+import store from "../src/app/store"; // Import your Redux store
+import App from "./App"; // Import your root component
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <App />
   </Provider>
-  
 );
 
 // If you want to start measuring performance in your app, pass a function
