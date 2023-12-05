f4c1dc56a9a4e15268e5b1591b786dd1fd550852
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:41:32 -0600

    Reformatting code

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -99,9 +99,17 @@ export default function Movies() {
                       className="movies__details"
                       onClick={() => {
                         if (movie.title) {
-                          navigate(`/movie/${movie.id}/${encodeURIComponent(movie.title)}`);
+                          navigate(
+                            `/movie/${movie.id}/${encodeURIComponent(
+                              movie.title
+                            )}`
+                          );
                         } else if (movie.name) {
-                          navigate(`/show/${movie.id}/${encodeURIComponent(movie.name)}`);
+                          navigate(
+                            `/show/${movie.id}/${encodeURIComponent(
+                              movie.name
+                            )}`
+                          );
                         }
                       }}
                     >
