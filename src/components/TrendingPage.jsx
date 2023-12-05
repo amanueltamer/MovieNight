f3309d26f3e4790a5a44c506acc2c1e686cb486e
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:42:29 -0600

    Reformatting code

--- src/components/TrendingPage.jsx
+++ src/components/TrendingPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Header from './Header'
-import Trending from './Trending'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import Trending from "./Trending";
+import Footer from "./Footer";
 
 export default function TrendingPage() {
   return (
-    <div className='trending__page'>
-        <Header />
-        <Trending />
-        <Footer />
+    <div className="trending__page">
+      <Header />
+      <Trending />
+      <Footer />
     </div>
-  )
+  );
 }
