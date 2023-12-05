43007ec99c2427abe62b0f7ba15ec7ee83b9b9da
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:40:20 -0600

    Reformatting code

--- src/App.jsx
+++ src/App.jsx
@@ -10,24 +10,23 @@ import SearchPage from "./components/SearchPage";
 import DiscoverPage from "./components/DiscoverPage";
 import PeopleDetailsPage from "./components/PeopleDetailsPage";
 
-
 function App() {
   return (
     <Router>
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/trending" element={<TrendingPage />}></Route>
-        <Route path="/discover" element={<DiscoverPage />}></Route>
-        <Route path="/people" element={<PeoplePage />}></Route>
-        <Route path="/upcoming" element={<Upcomingpage />}></Route>
-        <Route path="/topRated" element={<TopRatedPage />}></Route>
-        <Route path="/popular" element={<PopularPage />}></Route>
-        <Route path="/:type/:id/:title" element={<MovieDetailsPage />} />
-        <Route path="/person/:id?" element={<PeopleDetailsPage />} />
-        <Route path="/search/:query?" element={<SearchPage />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/trending" element={<TrendingPage />}></Route>
+          <Route path="/discover" element={<DiscoverPage />}></Route>
+          <Route path="/people" element={<PeoplePage />}></Route>
+          <Route path="/upcoming" element={<Upcomingpage />}></Route>
+          <Route path="/topRated" element={<TopRatedPage />}></Route>
+          <Route path="/popular" element={<PopularPage />}></Route>
+          <Route path="/:type/:id/:title" element={<MovieDetailsPage />} />
+          <Route path="/person/:id?" element={<PeopleDetailsPage />} />
+          <Route path="/search/:query?" element={<SearchPage />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
