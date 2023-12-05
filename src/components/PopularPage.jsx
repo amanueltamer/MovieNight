3be240f624a9f3a568909b39b81e47b05ef4e9c4
amanueltamer <amanueltamer@gmail.com>
2023-12-05 16:42:01 -0600

    Reformatting code

--- src/components/PopularPage.jsx
+++ src/components/PopularPage.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import Footer from './Footer'
-import Header from './Header'
-import Popular from './Popular'
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Popular from "./Popular";
 
 export default function PopularPage() {
   return (
-    <div className='popularPage'>
-        <Header />
-        <Popular />
-        <Footer />
+    <div className="popularPage">
+      <Header />
+      <Popular />
+      <Footer />
     </div>
-  )
+  );
 }
