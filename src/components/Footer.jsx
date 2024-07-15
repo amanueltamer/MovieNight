b8ed248df57d2dd89143c9e847b5582890a5f366
amanueltamer <amanueltamer@gmail.com>
2024-07-15 10:28:04 -0500

    Updated for the new year

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="footer">
-      <p className="footer__center">All rights reserved © 2023 Amanuel Tamer</p>
+      <p className="footer__center">All rights reserved © 2024 Amanuel Tamer</p>
     </div>
   );
 };
